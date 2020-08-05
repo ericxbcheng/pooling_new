12e849357d5ea7e50bee5992d77df8ea63b34f79
Eric Cheng <34377439+ericxbcheng@users.noreply.github.com>
2020-08-05 15:25:08 -0500

    Adapted functions from ILSI

--- Pooling_covid.R
+++ Pooling_covid.R
@@ -2,3 +2,13 @@ library(tidyverse)
 source("Pooling_covid_functions.R")
 
 
+n_iter = 10
+Args_default = list(n = 20, n_pos = 2, pool_size = 5, thresh = 0)
+
+sim_result = sim_iterate_covid(n_iter = n_iter, Args = Args_default)
+sim_result_cleaned = clean_covid(list = sim_result)
+
+tune_param_covid(Args = Args_default, n_iter = n_iter, param = "n_pos", val = 5)
+tune_param_n_covid(vals = c(1,3,5), Args = Args_default, n_iter = 10, var_prim = "n_pos")
+tune_param_sec_covid(Args = Args_default, var_prim = "n_pos", vals_prim = c(1,3,5), 
+                     var_sec = "pool_size", n_iter = 10, vals_sec = c(2,4,6))
--- Pooling_covid_functions.R
+++ Pooling_covid_functions.R
@@ -1,9 +1,9 @@
-make_samples = function(n, pos){
+make_samples = function(n, n_pos){
   
-  stopifnot(n >= pos)
+  stopifnot(n >= n_pos)
   
-  a = rep.int(x = 1, times = pos)
-  b = vector(mode = "numeric", length = n - pos)
+  a = rep.int(x = 1, times = n_pos)
+  b = vector(mode = "numeric", length = n - n_pos)
   
   return(sample(x = c(a,b), size = n, replace = FALSE))
 }
@@ -24,7 +24,7 @@ make_pool_idx = function(n, pool_size){
   }
 }
 
-find_pool_pos = function(conc, pool_idx, thresh){
+find_pool_pos_covid = function(conc, pool_idx, thresh){
   
   # Calculate pooled concentration
   pools = split(x = conc, f = pool_idx) %>%
@@ -36,8 +36,13 @@ find_pool_pos = function(conc, pool_idx, thresh){
 
 calc_1d_metrics_covid = function(n, conc, pool_pos, pool_idx){
   
-  class_true = {conc == 1}
-  class_pred = ifelse(test = pool_idx %in% which(pool_pos), yes = TRUE, no = FALSE)
+  class_true = {conc == 1} %>%
+    as.numeric() %>%
+    factor(x = ., levels = c(0, 1))
+  
+  class_pred = ifelse(test = pool_idx %in% which(pool_pos), yes = TRUE, no = FALSE) %>%
+    as.numeric() %>%
+    factor(x = ., levels = c(0, 1))
   
   # Make a contingency table and calculate sensitivity and specificity
   cont_table = table(class_true, class_pred)
@@ -49,3 +54,136 @@ calc_1d_metrics_covid = function(n, conc, pool_pos, pool_idx){
   
   return(out)
 }
+
+sim_1d_outcome_covid = function(n, n_pos, pool_size, thresh = 0){
+  
+  stopifnot(n > n_pos | n > 0 | n_pos >= 0)
+  
+  conc = make_samples(n = n, n_pos = n_pos)
+  
+  pool_idx = make_pool_idx(n = n, pool_size = pool_size)
+  
+  pool_pos = find_pool_pos_covid(conc = conc, pool_idx = pool_idx, thresh = thresh)
+  
+  # Calculate sensitivity and specificity
+  out = calc_1d_metrics_covid(n = n, conc = conc, pool_pos = pool_pos, pool_idx = pool_idx)
+  
+  return(out)
+}
+
+# Iterate once
+gen_sim_1d_outcome_covid = function(n, n_pos, thresh, pool_size){
+  function(...){
+    sim_1d_outcome_covid(n = n, n_pos = n_pos, thresh = thresh, pool_size = pool_size)
+  }
+}
+
+# First layer of iteration
+sim_iterate_covid = function(n_iter, Args){
+  
+  # Check point: Is n_iter >= 1?
+  stopifnot(n_iter >= 1)
+  
+  # Generate a sim_outcome_new() with loaded arguments
+  a = do.call(what = gen_sim_1d_outcome_covid, args = Args)
+  
+  # Iterate that manufactured function for n_iter times
+  b = map(.x = 1:n_iter, .f = a)
+  
+  return(b)
+}
+
+clean_covid = function(list){
+  a = unlist(list)
+  
+  # Find the indices of each type of metric
+  sens_ind = seq(from = 1, by = 3, length.out = length(a)/3)
+  spec_ind = sens_ind + 1
+  
+  #Rearrange the results
+  b = list("sensi" = a[sens_ind], "speci" = a[spec_ind])
+  
+  return(b)
+}
+
+# This function makes a list of arguments from the default arguments
+update_arg_covid = function(Args, param, val){
+  
+  # Checkpoint
+  stopifnot(param %in% names(Args))
+  
+  Args[[param]] = val
+  return(Args)
+}
+
+# Create a function that passes one tuning parameter value to sim_iterate2()
+tune_param_covid = function(Args, n_iter, param, val, ...){
+  
+  # Get tuning parameters
+  a = update_arg_covid(Args = Args, param = param, val = val)
+  
+  b = sim_iterate_covid(n_iter = n_iter, Args = a) %>%
+    clean_covid(list = .)
+  
+  # Add a vector of tuning parameter value
+  b$param = rep.int(x = val, times = n_iter)
+  
+  return(b)
+}
+
+# A function that tunes over many values of the primary tuning parameter
+tune_param_n_covid = function(vals, Args, n_iter, var_prim){
+  return(map(.x = vals, .f = tune_param_covid, Args = Args, n_iter = n_iter, param = var_prim))
+}
+
+
+# A function that tunes over a crossed combination of the primary and secondary tuning parameter
+tune_param_sec_covid = function(Args, var_prim, vals_prim, var_sec, n_iter, vals_sec){
+  
+  # Create a list of argument lists, each argument list corresponding to one secondary tuning value
+  Args_sec = map(.x = vals_sec, .f = update_arg_covid, Args = Args, param = var_sec)
+  
+  # For each argument list in Args_sec, do iterate_tune1()
+  sim_data = list()
+  for(i in 1:length(vals_sec)){
+    
+    sim_data[[i]] = map(.x = vals_prim, .f = tune_param_covid, 
+                        Args = Args_sec[[i]], n_iter = n_iter,param = var_prim)
+    
+  }
+  return(sim_data)
+}
+
+# # Calculate the cost of reagents and pipetting
+# calc_1d_cost = function(data, n, by){
+#   
+#   stopifnot(by %in% c("row", "column"))
+#   stopifnot(n %in% c(48, 96))
+#   
+#   if(n == 48){
+#     n_row = 6
+#     n_col = 8
+#   } else if(n == 96){
+#     n_row = 8
+#     n_col = 12
+#   } else {
+#     stop("Undefined n. Choose 48 or 96.")
+#   }
+#   
+#   if(by == "row"){
+#     a = do.call(what = rbind.data.frame, args = data) %>%
+#       mutate(putative_pos = sensi*n_pos + (1-speci)*(n-n_pos),
+#              n_test_total = n_row + putative_pos,
+#              n_pipette = n + n_row + putative_pos)
+#     return(a)
+#     
+#   } else if(by == "column"){
+#     b = do.call(what = rbind.data.frame, args = data) %>%
+#       mutate(putative_pos = sensi*n_pos + (1-speci)*(n-n_pos),
+#              n_test_total = n_col + putative_pos,
+#              n_pipette = n + n_col + putative_pos)
+#     return(b)
+#     
+#   }
+# }
+
